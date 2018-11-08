e5252c5fe87faf1b4529a5f82760a22f0e0814c1
Johannes Drexler <jdrexler94@gmx.de>
2018-11-08 19:37:12 +0100

    *added some TODOs

---
+++ src/Builds.ps1
@@ -0,0 +1,4 @@
+#TODO: Get-BuildDefinitions (projectNameOrId as optional parameter)
+#TODO: Remove-BuildDefinition (projectNameOrId, definitionId)
+#TODO: Get-Builds (projectNameOrId, definitionId)
+#TODO: Remove-Build (projectNameOrId, buildId)
